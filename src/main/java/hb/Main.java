f4bef1cc3c9d32ba85db5ca492580cd509e30418
alexy6574 <alexythouin@outlook.com>
2024-09-09 13:17:19 -0400

    Init Create

--- src/main/java/hb/Main.java
+++ src/main/java/hb/Main.java
@@ -1,8 +1,10 @@
 package hb;
 
+import com.sun.source.tree.Scope;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.BreakIterator;
+
 import java.util.Scanner;
 
 /**
@@ -13,6 +15,7 @@ import java.util.Scanner;
 public class Main {
 
     public static int Quiz() {
+        int resultat = 0;
         try {
             File fichier = new File("src/main/resources/input.txt");
             Scanner lecteur = new Scanner(fichier);
@@ -20,13 +23,23 @@ public class Main {
             while (lecteur.hasNextLine()) {
                 String ligne = lecteur.nextLine();
                 System.out.println(ligne);
+                StringBuilder num = new StringBuilder();
+
+
+                for (char c : ligne.toCharArray()) {
+                    if (Character.isDigit(c)) {
+                        num.append(c);
+                    }
+                }
+
+                resultat += Integer.parseInt(String.valueOf(num.toString().charAt(0)) + num.toString().charAt(num.length() - 1));
             }
 
             lecteur.close();
         } catch (FileNotFoundException e) {
             System.out.println("Le fichier n'a pas été trouvé.");
         }
-        return 0;
+        return resultat;
     }
 
     public static void main(String[] args) {
@@ -34,7 +47,7 @@ public class Main {
         if (result == 54159) {
             System.out.println("BRAVO");;
         } else {
-            System.out.println("Incorrect answer: " + result + "The expected output is 54159");
+            System.out.println("Incorrect answer: " + result + " The expected output is 54159");
         }
     }
 }
